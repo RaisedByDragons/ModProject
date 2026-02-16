749b08666ef85dc337917513e9d47fb4709f65d9
Cayden <caydenjr889@gmail.com>
2026-02-16 16:49:47 -0500

    Added ModItems class for registering items

--- FabricMod/src/main/java/com/mod/Mod.java
+++ FabricMod/src/main/java/com/mod/Mod.java
@@ -1,11 +1,12 @@
 package com.mod;
 
+import com.mod.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ModInit implements ModInitializer {
+public class Mod implements ModInitializer {
 	public static final String MOD_ID = "mod";
 
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
@@ -16,7 +17,6 @@ public class ModInit implements ModInitializer {
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
-
-
+		ModItems.registerModItems();
 	}
 }
--- FabricMod/src/main/java/com/mod/items.java
+++
@@ -1,5 +0,0 @@
-package com.mod;
-
-public class items {
-
-}
