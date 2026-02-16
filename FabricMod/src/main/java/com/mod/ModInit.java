718803b7dee79d2c1d96400a43a6f54a6b0ff85b
Cayden <caydenjr889@gmail.com>
2026-02-16 16:13:06 -0500

    Renaming files

--- FabricMod/src/main/java/com/mod/ModInit.java
+++ FabricMod/src/main/java/com/mod/ModInit.java
@@ -1,16 +1,13 @@
-package com.example;
+package com.mod;
 
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ExampleMod implements ModInitializer {
-	public static final String MOD_ID = "modid";
+public class ModInit implements ModInitializer {
+	public static final String MOD_ID = "mod";
 
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
@@ -19,6 +16,7 @@ public class ExampleMod implements ModInitializer {
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
-		LOGGER.info("Hello Fabric world!");
+
+
 	}
 }
